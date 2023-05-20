a2988daf8d0e437196ecd57a66a49100d81a5e46
Franco <francolens2002@gmail.com>
2023-05-20 18:16:20 -0300

    sadda

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// Import all of Bootstrap's JS
+import * as bootstrap from '../../node_modules/bootstrap/dist/js/bootstrap'
+import * as Popper from '/node_modules/@popperjs/core/dist/umd/popper.js'
+
+
+let a = prompt("Prueba");
+alert(a);
